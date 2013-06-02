889430bf4939504a62cbced0b61faad56651c11f
crazy-max <crazyymaxx@gmail.com>
2013-06-02 19:37:53 +0200

    Use wget instead of curlftpfs
    Adding license

--- README.md
+++ README.md
@@ -4,15 +4,18 @@ FTP Sync
 A shell script to synchronize files between a remote FTP server and your local server/computer.<br />
 A file containing the MD5 hash of the name of each downloaded file will prevent re-download a file even if it is not present in the destination directory.<br />
 You can also apply a filter to search for files with a regular expression.<br />
-Ideal for those with a seedbox or a shared seedbox...<br />
-Tested on Debian and Ubuntu.
+Because this script only need ``wget``, it is ideal for those with a seedbox or a shared seedbox to synchronize with a NAS (Synology Qnap D-Link) or a local computer...
 
 Requirements
 ------------
 
-* [curlftpfs](http://curlftpfs.sourceforge.net/) is required.
+Commands :
+
 * [awk](http://en.wikipedia.org/wiki/Awk) is required.
+* [mail](http://linux.die.net/man/1/mail) is optional if you do not fill EMAIL_LOG.
 * [md5sum](http://en.wikipedia.org/wiki/Md5sum) is required.
+* [nawk](http://linux.die.net/man/1/nawk) is required.
+* [wget](http://en.wikipedia.org/wiki/Wget) is required.
 
 Installation
 ------------
@@ -35,9 +38,10 @@ Before running the script, you must change some vars.
 * **DL_USER** - Linux owner user of downloaded files. Optional.
 * **DL_GROUP** - Linux owner group of downloaded files. Optional.
 * **DL_CHMOD** - Permissions of downloaded files. Optional. (e.g. 644)
-* **DL_PATTERN** - Apply a filter to search for files with a regular expression. Separate each regular expression with a semicolon. Optional. (e.g. Game.Of.Thrones*.avi;Burn.Notice.*.avi)
+* **DL_REGEX** - Apply a filter to search for files with a regular expression. Separate each regular expression with a semicolon. Leave empty to grab all files. Optional. (e.g. Game.Of.Thrones*.avi;Burn.Notice.*.avi)
 * **DL_RETRY** - Number of retries in case of failure of download. (default 3)
 * **DL_HIDE_SKIPPED** - Not display the downloads already made ​​or valid in logs. (default 0)
+* **DL_HIDE_PROGRESS** - Not display the progress dots during downloads. (default 1)
 * **MD5_ENABLED** - Enable audit file already downloaded.
 * **MD5_FILE** - The audit file containing the hash of each downloaded file (default /etc/ftp-sync/ftp-sync.md5).
 * **DIR_LOGS** - Path to save ftp-sync logs. (default /etc/ftp-sync/logs)
@@ -68,24 +72,25 @@ Each time the script is executed, a log file is created.
 Here is an example :
 
 ```console
-FTP Sync v1.0 (2013/05/23 17:07:07)
-Connecting to 10.0.0.1:21...
+FTP Sync v1.3 (2013/06/02 19:00:16)
+Script PID: 32017
 Source: ftp://10.0.0.1:21/complete/
 Destination: /tmp/seedbox/
-Log file: /etc/ftp-sync/logs/ftp-sync-20130523170707.log
+Log file: /etc/ftp-sync/logs/ftp-sync-20130602190016.log
 MD5 file: /etc/ftp-sync/ftp-sync.md5
 --------------
 Finding files...
+Regex: ^.*$
 --------------
 Process file : Burn.Notice.S06E16.VOSTFR.HDTV.XviD.avi
 Hash: 5cc4931d64bd5579e46041b7139bde9f
-Size: 351M
+Size: 351 Mb
 Status : Already downloaded and valid. Skip download...
 Time spent: 00:00:00
 --------------
 Process file : Burn.Notice.S06E17E18.VOSTFR.HDTV.XviD.avi
 Hash: 5cc4931d64bd5579e46041b7139bde9f
-Size: 703M
+Size: 703 Mb
 Status : Already downloaded and valid. Skip download...
 Time spent: 00:00:00
 --------------
@@ -97,3 +102,13 @@ The MD5 file looks like this :
 baf87b6719e9f5499627fc8691efbd3c Burn.Notice.S06E16.VOSTFR.HDTV.XviD.avi
 92d1d13049bd148f89ffa1d501f153f5 Burn.Notice.S06E17E18.VOSTFR.HDTV.XviD.avi
 ```
+
+License
+-------
+
+LGPL. See ``LICENSE`` for more details.
+
+More infos
+----------
+
+http://www.crazyws.fr/dev/systeme/synchroniser-votre-seedbox-avec-votre-nas-ou-votre-ordinateur-6NGGE.html
--- ftp-sync.sh
+++ ftp-sync.sh
@@ -10,7 +10,7 @@
 
 ##################################################################################
 #                                                                                #
-#  FTP Sync v1.2                                                                 #
+#  FTP Sync v1.3                                                                 #
 #                                                                                #
 #  A shell script to synchronize files between a remote FTP server and           #
 #  your local server/computer.                                                   #
@@ -18,25 +18,26 @@
 #  prevent re-download a file even if it is not present in the destination       #
 #  directory.                                                                    #
 #  You can also apply a filter to search for files with a regular expression.    #
-#  Ideal for those with a seedbox or a shared seedbox...                         #
-#  Tested on Debian and Ubuntu.                                                  #
+#  Because this script only need wget, it is ideal for those with a seedbox      #
+#  or a shared seedbox to synchronize with a NAS (Synology Qnap D-Link) or a     #
+#  local computer...                                                             #
 #                                                                                #
-#  Author: Cr@zy                                                                 #
-#  Contact: http://www.crazyws.fr                                                #
+#  Copyright (C) 2013 Cr@zy <webmaster@crazyws.fr>                               #
 #                                                                                #
-#  This program is free software: you can redistribute it and/or modify it       #
-#  under the terms of the GNU General Public License as published by the Free    #
-#  Software Foundation, either version 3 of the License, or (at your option)     #
-#  any later version.                                                            #
+#  FTP Sync is free software; you can redistribute it and/or modify              #
+#  it under the terms of the GNU Lesser General Public License as published by   #
+#  the Free Software Foundation, either version 3 of the License, or             #
+#  (at your option) any later version.                                           #
 #                                                                                #
-#  This program is distributed in the hope that it will be useful, but WITHOUT   #
-#  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS #
-#  FOR A PARTICULAR PURPOSE. See the GNU General Public License for more         #
-#  details.                                                                      #
+#  FTP Sync is distributed in the hope that it will be useful,                   #
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of                #
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                  #
+#  GNU Lesser General Public License for more details.                           #
 #                                                                                #
-#  You should have received a copy of the GNU General Public License along       #
-#  with this program.  If not, see http://www.gnu.org/licenses/.                 #
+#  You should have received a copy of the GNU Lesser General Public License      #
+#  along with this program. If not, see http://www.gnu.org/licenses/.            #
 #                                                                                #
+#  Related post: http://goo.gl/OcJFA                                             #
 #  Usage: ./ftp-sync.sh DIR_DEST                                                 #
 #                                                                                #
 ##################################################################################
@@ -52,9 +53,10 @@ FTP_SRC="/"
 DL_USER=""
 DL_GROUP=""
 DL_CHMOD=""
-DL_PATTERN=""
+DL_REGEX=""
 DL_RETRY=3
 DL_HIDE_SKIPPED=0
+DL_HIDE_PROGRESS=1
 
 # MD5
 MD5_ENABLED=1
@@ -70,21 +72,22 @@ EMAIL_LOG=""
 
 function isDownloaded() {
   local srcfile="$1"
-  local srcfiletr=`echo -n "$srcfile" | sed -e "s#$DIR_SRC##g"`
+  local srcfiledec=$(urlDecode "$srcfile")
+  local srcfiletr=`echo -n "$srcfiledec" | sed -e "s#$FTP_SRC##g" | cut -c1-`
   local srchash=`echo -n "$srcfiletr" | md5sum - | cut -d ' ' -f 1`
-  local srcsize=`ls -la "$srcfile" | awk '{ print $5}'`
+  local srcsize=$(getSize "$srcfile")
 
   # Check skip MD5
-  if [ -z "$2" ]; then local skipmd5=0; else local skipmd5=1; fi
+  if [ -z "$2" ]; then local skipmd5=0; else local skipmd5=$2; fi
   
-  local destfile=`echo "$srcfile" | sed -e "s#$DIR_SRC#$DIR_DEST#g"`
+  local destfile=`echo "$srcfiledec" | sed -e "s#$FTP_SRC#$DIR_DEST#g"`
   if [ -f "$destfile" ]
   then
     local destsize=`ls -la "$destfile" | awk '{ print $5}'`
     if [ "$srcsize" == "$destsize" ]
     then
       echo "1"
-      if [ "$MD5_ACTIVATED" == "1" -a "$skipmd5" == "0" -a -z "`grep "$srchash" "$MD5_FILE"`" ]
+      if [ "$MD5_ACTIVATED" == "1" -a "$skipmd5" == "0" -a -z "`grep "^$srchash" "$MD5_FILE"`" ]
       then
         echo "$srchash $srcfiletr" >> "$MD5_FILE"
       fi
@@ -115,7 +118,8 @@ function isMd5Enabled() {
 
 function downloadFile() {
   local srcfile="$1"
-  local srcfiletr=`echo -n "$srcfile" | sed -e "s#$DIR_SRC##g"`
+  local srcfiledec=$(urlDecode "$srcfile")
+  local srcfiletr=`echo -n "$srcfiledec" | sed -e "s#$FTP_SRC# #g" | cut -c1-`
   local srchash=`echo -n "$srcfiletr" | md5sum - | cut -d ' ' -f 1`
   local destfile="$2"
   local hidelog="$3"
@@ -125,7 +129,7 @@ function downloadFile() {
 
   # Create destfile path if does not exist
   local destpath="${destfile%/*}"
-  if [ ! -d $destpath ]
+  if [ ! -d "$destpath" ]
   then
     mkdir -p "$destpath"
     changePerms "$destpath"
@@ -133,11 +137,11 @@ function downloadFile() {
 
   # Begin download
   if [ -z "$LOG" ]; then echo "Start download to $destfile... Please wait..."; fi
-  local errorcp=`cp "$srcfile" "$destfile" 2>&1`
-  if [ -z "$LOG" -a ! -z "$errorcp" ]; then echo "ERROR: $errorcp"; fi
-
+  wget --progress=dot:mega --ftp-user="$FTP_USER" --ftp-password="$FTP_PASSWORD" -O "$destfile" "ftp://$FTP_HOST:$FTP_PORT$srcfile" 2>&1 | progressFilter
+  
+  local errordl="$?"
   local dlstatus=`isDownloaded "$srcfile" "1"`
-  if [ -z "$errorcp" -a ${dlstatus:0:1} -eq 1 ]
+  if [ $errordl == 0 -a ${dlstatus:0:1} -eq 1 ]
   then
     if [ -z "$LOG" ]; then echo "File successfully downloaded!"; fi
     changePerms "$destfile"
@@ -150,52 +154,53 @@ function downloadFile() {
     if [ $retry -lt $DL_RETRY ]
     then
       retry=`expr $retry + 1`
-      if [ -z "$LOG" ]; then echo "ERROR: Download failed... retry $retry/3"; fi
+      if [ -z "$LOG" ]; then echo "ERROR $errordl${dlstatus:0:1}: Download failed... retry $retry/3"; fi
       downloadFile "$srcfile" "$destfile" "$hidelog" "$retry"
     else
-      if [ -z "$LOG" ]; then echo "ERROR: Download failed and too many retries..."; fi
+      if [ -z "$LOG" ]; then echo "ERROR $errordl${dlstatus:0:1}: Download failed and too many retries..."; fi
     fi
   fi
 }
 
-function mountFtp() {
-  local mountpoint="/mnt/ftp-sync"
-  if grep -qs "$mountpoint" /proc/mounts; then umountFtp; fi
-  if [ ! -d $mountpoint ]; then mkdir -p $mountpoint; fi
-
-  echo "Connecting to $FTP_HOST:$FTP_PORT..."
-  local status=$(curlftpfs "$FTP_USER:$FTP_PASSWORD@$FTP_HOST:$FTP_PORT" "$mountpoint" -o nonempty 2>&1)
-  if [ "$status" != "" ]; then echo "ERROR: $status"; exit 1; fi
-
-  DIR_SRC="$mountpoint$FTP_SRC"
-}
-
-function umountFtp() {
-  local mountpoint="/mnt/ftp-sync"
-  umount "$mountpoint"
-  rmdir "$mountpoint"
+function findFiles() {
+  local path="$1"
+  local regex="$2"
+  local address="ftp://$FTP_HOST:$FTP_PORT"
+  local files=$(wget -q --ftp-user="$FTP_USER" --ftp-password="$FTP_PASSWORD" -O - "$address$path" | grep -o 'ftp:[^"]*')
+  while read -r line
+  do
+    local file=$(echo "$line" | sed "s#&\#32;#%20#g" | sed "s#$address# #g" | cut -c2-)
+    local filedec=$(urlDecode "$file")
+    local filetr=`echo -n "$filedec" | sed -e "s#$FTP_SRC# #g" | cut -c2-`
+    local vregex=`echo -n "$filetr" | sed -n "/$regex/p"`
+    if [ "${file#${file%?}}" == "/" ]
+    then
+      findFiles "$file" "$regex"
+    elif [ ! -z "$vregex" ]
+    then
+      echo "$file"
+    fi
+  done <<< "$files"
 }
 
 function process() {
-  local pattern="$1"
+  local regex="$1"
   echo "Finding files..."
-  echo "Regexp: $pattern"
+  echo "Regex: $regex"
   echo "--------------"
-  find "$DIR_SRC" -name "$pattern" -type f | sort | while read srcfile
+  findFiles "$FTP_SRC" "$regex" | sort | while read srcfile
   do
     LOG=""
     local skipdl=0
+    local srcfiledec=$(urlDecode "$srcfile")
     local starttime=$(awk 'BEGIN{srand();print srand()}')
-    local srcfiletr=`echo -n "$srcfile" | sed -e "s#$DIR_SRC##g"`
+    local srcfiletr=`echo -n "$srcfiledec" | sed -e "s#$FTP_SRC##g" | cut -c1-`
 
     # Start process on a file
     addLog "Process file : $srcfiletr"
     local srchash=`echo -n "$srcfiletr" | md5sum - | cut -d ' ' -f 1`
     addLog "Hash: $srchash"
-
-    # File size
-    local srcsize=`ls -lah "$srcfile" | awk '{ print $5}'`
-    addLog "Size: $srcsize"
+    addLog "Size: $(getHumanSize "$srcfile")"
 
     # Check validity
     local dlstatus=`isDownloaded "$srcfile"`
@@ -221,7 +226,7 @@ function process() {
 
     if [ "$skipdl" == "0" ]
     then
-      local destfile=`echo "$srcfile" | sed -e "s#$DIR_SRC#$DIR_DEST#g"`
+      local destfile=`echo "$srcfiledec" | sed -e "s#$FTP_SRC#$DIR_DEST#g"`
       downloadFile "$srcfile" "$destfile" "$hidelog"
     fi
 
@@ -230,6 +235,74 @@ function process() {
     if [ -z "$LOG" ]; then echo "Time spent: `formatSeconds $(($endtime - $starttime))`"; fi
     if [ -z "$LOG" ]; then echo "--------------"; fi
   done
+}
+
+function progressFilter() {
+  if [ "$DL_HIDE_PROGRESS" == "0" ]
+  then
+    local flag=2 c count cr=$'\r' nl=$'\n'
+    while IFS='' read -d '' -rn 1 c
+    do
+      if [ $flag == 1 ]
+      then
+        printf '%c' "$c"
+        if [[ "$c" =~ (s$) ]]
+        then
+          flag=0
+          echo ""
+        fi
+      elif [ $flag != 0 ]
+      then
+        if [[ $c != $cr && $c != $nl ]]
+        then
+          count=0
+        else
+          ((count++))
+          if ((count > 1))
+          then
+            flag=1
+          fi
+        fi
+      fi
+    done
+  fi
+}
+
+function watchTail() {
+  local cur_pid=$$
+  local tail_args=`echo "tail -f $LOG_FILE" | cut -c1-79`
+  local pid=`ps -e -o pid,ppid,args | grep ${cur_pid} | grep "${tail_args}"| grep -v grep | nawk '{print $1}'`
+
+  if [ "$pid" = "" ]
+  then
+    if [ -z "$PS1" ]; then exit 0; else return 0; fi
+  fi
+
+  local ppid=2
+  while [ "$ppid" != "1" ]
+  do
+     local pids=`ps -e -o pid,ppid,args | grep "${tail_args}"| grep ${pid} | grep -v grep | nawk '{print $1"-"$2}'`
+     if [ "$pids" == "" ]; then break; fi
+     local ppid=`echo ${pids} | nawk -F- '{print $2}'`
+     if ((ppid==1))
+     then
+       if [ ! -z "$EMAIL_LOG" ]; then cat "$LOG_FILE" | mail -s "ftp-sync on $(hostname)" $EMAIL_LOG; fi
+       sleep 3
+       kill -9 $pid
+     fi
+  done
+}
+
+function urlDecode() {
+  echo "$1" | sed -e "s/%\([0-9A-F][0-9A-F]\)/\\\\\x\1/g" | xargs -0 echo -e
+}
+
+function getSize() {
+  echo $(wget -S --spider --ftp-user="$FTP_USER" --ftp-password="$FTP_PASSWORD" -O - "ftp://$FTP_HOST:$FTP_PORT$1" >&1 2>&1 | grep '^213' | awk '{print $2}')
+}
+
+function getHumanSize() {
+  echo $(getSize "$1") | awk '{ sum=$1 ; hum[1024**3]="Gb";hum[1024**2]="Mb";hum[1024]="Kb"; for (x=1024**3; x>=1024; x/=1024){ if (sum>=x) { printf "%.2f %s\n",sum/x,hum[x];break } }}'
 }
 
 function changePerms() {
@@ -255,31 +328,6 @@ function rebuildPath() {
   if [ "${path:len}" != "/" ]; then path="$path/"; fi
   if [ "${path:0:1}" != "/" ]; then path="/$path"; fi
   echo "$path"
-}
-
-function watchTail() {
-  cur_pid=$$
-  tail_args=`echo "tail -f $LOG_FILE" | cut -c1-79`
-  pid=`ps -e -o pid,ppid,args | grep ${cur_pid} | grep "${tail_args}"| grep -v grep | nawk '{print $1}'`
-
-  if [ "$pid" = "" ]
-  then
-    if [ -z "$PS1" ]; then exit 0; else return 0; fi
-  fi
-
-  ppid=2
-  while [ "$ppid" != "1" ]
-  do
-     pids=`ps -e -o pid,ppid,args | grep "${tail_args}"| grep ${pid} | grep -v grep | nawk '{print $1"-"$2}'`
-     if [ "$pids" == "" ]; then break; fi
-     ppid=`echo ${pids} | nawk -F- '{print $2}'`
-     if ((ppid==1))
-     then
-       if [ ! -z "$EMAIL_LOG" ]; then cat "$LOG_FILE" | mail -s "ftp-sync on $(hostname)" $EMAIL_LOG; fi
-       sleep 3
-       kill -9 $pid
-     fi
-  done
 }
 
 function addLog() {
@@ -319,19 +367,16 @@ fi
 # Starting watch in background and process
 watchTail &
 
-echo "FTP Sync v1.2 (`date +"%Y/%m/%d %H:%M:%S"`)"
+echo "FTP Sync v1.3 (`date +"%Y/%m/%d %H:%M:%S"`)"
 
 # Check required packages
 if [ ! -x `which awk` ]; then echo "ERROR: You need awk for this script (try apt-get install awk)"; exit 1; fi
 if [ ! -x `which md5sum` ]; then echo "ERROR: You need md5sum for this script (try apt-get install md5sum)"; exit 1; fi
-if [ ! -x `which curlftpfs` ]; then echo "ERROR: You need curlftpfs for this script (try apt-get install curlftpfs)"; exit 1; fi
-
-# Mount FTP
-mountFtp
-if [ "$?" == "1" ]; then exit 1; fi
+if [ ! -x `which nawk` ]; then echo "ERROR: You need nawk for this script (try apt-get install nawk)"; exit 1; fi
+if [ ! -x `which wget` ]; then echo "ERROR: You need wget for this script (try apt-get install wget)"; exit 1; fi
 
 # Check directories
-if [ ! -d "$DIR_SRC" ]; then echo "ERROR: $DIR_SRC is not a directory"; exit 1; else DIR_SRC=`rebuildPath "$DIR_SRC"`; fi
+FTP_SRC=`rebuildPath "$FTP_SRC"`
 if [ ! -d "$DIR_DEST" ]; then mkdir -p "$DIR_DEST"; fi; DIR_DEST=`rebuildPath "$DIR_DEST"`
 
 # Check MD5 file
@@ -354,17 +399,14 @@ echo "--------------"
 # Start process
 starttime=$(awk 'BEGIN{srand();print srand()}')
 
-if [ -z "$DL_PATTERN" ]; then DL_PATTERN="*;"; fi
-IFS=';' read -ra PATTERN <<< "$DL_PATTERN"
-for p in "${PATTERN[@]}"; do
+if [ -z "$DL_REGEX" ]; then DL_REGEX="^.*$;"; fi
+IFS=';' read -ra REGEX <<< "$DL_REGEX"
+for p in "${REGEX[@]}"; do
   process "$p"
 done
 
 echo "Finished..."
 endtime=$(awk 'BEGIN{srand();print srand()}')
 echo "Total time spent: `formatSeconds $(($endtime - $starttime))`"
-
-# Umount FTP
-umountFtp
 
 exit 0
