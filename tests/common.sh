9344999f762d32f808777b92f8f6f43eaa27d96c
CrazyMax <crazy-max@users.noreply.github.com>
2017-04-25 23:33:01 +0200

    Begin tests (Issue #30)

---
+++ tests/common.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DEFAULT_CONFIG_FILE="/opt/ftpgrab/ftpgrab.conf"
+DEFAULT_DIR_DEST="/tmp/seedbox"
+DEFAULT_FTP_HOST="198.51.100.0"
+DEFAULT_FTP_PORT="21"
+DEFAULT_HASH_TYPE="md5"
+DEFAULT_HASH_STORAGE="text"
+
+# Public SFTP Servers are used for testing
+# http://www.sftp.net/public-online-sftp-servers
+
+SERVER1_IP="test.rebex.net"
+SERVER1_USER="demo"
+SERVER1_PASSWORD="password"
+
+SERVER2_IP="demo.wftpserver.com"
+SERVER2_USER="demo-user"
+SERVER2_PASSWORD="demo-user"
