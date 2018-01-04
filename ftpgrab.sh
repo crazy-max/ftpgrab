1943df1f1dd24c73a0b3e0cf77973d9c8e7bc2ee
CrazyMax <crazy-max@users.noreply.github.com>
2018-01-04 10:28:51 +0100

    New year

--- ftpgrab.sh
+++ ftpgrab.sh
@@ -7,7 +7,7 @@
 #  Simple script to grab your files from a remote FTP server.                     #
 #                                                                                 #
 #  MIT License                                                                    #
-#  Copyright (c) 2013-2017 CrazyMax                                               #
+#  Copyright (c) 2013-2018 CrazyMax                                               #
 #                                                                                 #
 #  Permission is hereby granted, free of charge, to any person obtaining a copy   #
 #  of this software and associated documentation files (the "Software"), to deal  #
